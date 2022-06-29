c32f45aec16ad62081bc0ccd0efe7af2e57a16a3
Angelica <Angelica137@users.noreply.github.com>
2022-06-29 18:57:01 +0100

    square must be between 1 and 64

--- scripts/grains.py
+++ scripts/grains.py
@@ -1,5 +1,5 @@
 def grains(square: int) -> int:
-    if square < 0 and square > 64:
+    if square < 1 or square > 64:
         raise ValueError("There are only 64 squares")
     return 1 << square - 1
 
