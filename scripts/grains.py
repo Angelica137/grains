28201ba3b00335900c96e604cae394bd10e99cfc
Angelica <Angelica137@users.noreply.github.com>
2022-06-29 18:37:46 +0100

    refactor grains()

--- scripts/grains.py
+++ scripts/grains.py
@@ -1,7 +1,7 @@
 def grains(square: int) -> int:
     if square < 0 and square > 64:
         raise ValueError("There are only 64 squares")
-    return 2**(square-1)
+    return 1 << square - 1
 
 
 def chessboard() -> int:
